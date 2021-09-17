b5deaf0623786c781eef0ad5d6f3a35114f1fc89
Ariel Beltran Paneque <abeltran.9476@gmail.com>
2021-09-17 18:45:43 -0400

    Soporte para 2 tipos de direcciones nuevos

--- lib/HD.php
+++ lib/HD.php
@@ -6,8 +6,6 @@ use BitWasp\Bitcoin\Address\AddressCreator;
 use BitWasp\Bitcoin\Bitcoin;
 use BitWasp\Bitcoin\Key\Deterministic\HdPrefix\GlobalPrefixConfig;
 use BitWasp\Bitcoin\Key\Deterministic\HdPrefix\NetworkConfig;
-use BitWasp\Bitcoin\Key\Factory\HierarchicalKeyFactory;
-use BitWasp\Bitcoin\Mnemonic\Bip39\Bip39SeedGenerator;
 use BitWasp\Bitcoin\Network\Slip132\BitcoinRegistry;
 use BitWasp\Bitcoin\Key\Deterministic\Slip132\Slip132;
 use BitWasp\Bitcoin\Key\KeyToScript\KeyToScriptHelper;
@@ -20,6 +18,8 @@ class HD
 
     private $network = NULL;
     private $xpub = NULL;
+    private $ypub = NULL;
+    private $zpub = NULL;
 
     public function set_network($network)
     {
@@ -31,10 +31,22 @@ class HD
         $this->xpub = $xpub;
     }
 
+    public function set_ypub($ypub)
+    {
+        $this->ypub = $ypub;
+    }
+
+    public function set_zpub($zpub)
+    {
+        $this->zpub = $zpub;
+    }
+
 
     public function address_from_master_pub($path = '0/0')
     {
-        $xpub= $this->xpub;
+
+
+
         $adapter = Bitcoin::getEcAdapter();
         $slip132 = new Slip132(new KeyToScriptHelper($adapter));
         $addrCreator = new AddressCreator();
@@ -44,26 +56,38 @@ class HD
         $btc = NetworkFactory::$network();
         $bitcoinPrefixes = new BitcoinRegistry();
 
+        if(!empty($this->xpub)){
+            $pub= $this->xpub;
+            $pubPrefix = $slip132->p2pkh($bitcoinPrefixes);
+        }
+
+        if(!empty($this->ypub)){
+            $pub= $this->ypub;
+            $pubPrefix = $slip132->p2shP2wpkh($bitcoinPrefixes);
+        }
+
+        if(!empty($this->zpub)){
+            $pub= $this->zpub;
+            $pubPrefix = $slip132->p2wpkh($bitcoinPrefixes);
+        }
+
         // Keys with ALL of these prefixes will be supported.
         // You can chose a subset if desired (for some networks it's
         // a good idea!)
         $config = new GlobalPrefixConfig([
             new NetworkConfig($btc, [
-            $slip132->p2pkh($bitcoinPrefixes)
+             $pubPrefix
             ])
         ]);
 
-        $btcPrefixConfig = $config->getNetworkConfig($btc);
-        $serializer = new Base58ExtendedKeySerializer(new ExtendedKeySerializer($adapter, $config));
+        $serializer = new Base58ExtendedKeySerializer(
+            new ExtendedKeySerializer($adapter, $config)
+          );
 
+        $key = $serializer->parse($btc, $pub);
+        $child_key = $key->derivePath($path);
 
-        // This shows how we create such keys. You
-        // don't actually need the config until serialize
-        // time
-        $hdFactory = new HierarchicalKeyFactory($adapter);
-        $p2shP2wshP2pkhKey = $hdFactory->fromExtended($xpub);
-        $addrKey = $p2shP2wshP2pkhKey->derivePath($path);
-        return $addrKey->getAddress($addrCreator)->getAddress($btc);
+        return $child_key->getAddress(new AddressCreator())->getAddress($btc);
     }
 
 }
